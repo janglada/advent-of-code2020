06ffa189795c91beacacd5d37d57b1e76b0e2f58
Joan Anglada <janglada@gmail.com>
2021-02-14 15:21:53 +0100

    day 24 2/2

--- src/day24.rs
+++ src/day24.rs
@@ -1,38 +1,67 @@
 extern crate peg;
 
+use itertools::Itertools;
 use std::collections::HashMap;
+use std::ops;
 
-pub enum Neighbour {
-    East,
-    SouthEast,
-    SouthWest,
-    West,
-    NorthWest,
-    NorthEast,
+#[derive(PartialEq, Eq, Hash, Debug, Copy, Clone)]
+pub struct Hex {
+    x: i32,
+    y: i32,
 }
 
-pub type Hex = (i32, i32);
+impl Hex {
+    pub fn new(x: i32, y: i32) -> Hex {
+        Hex { x, y }
+    }
+
+    pub fn neighbours(&self) -> Vec<Hex> {
+        vec![
+            *self + Hex::new(1, 0),
+            *self + Hex::new(0, 1),
+            *self + Hex::new(-1, 1),
+            *self + Hex::new(-1, 0),
+            *self + Hex::new(0, -1),
+            *self + Hex::new(1, -1),
+        ]
+    }
+}
+
+impl ops::Add<Hex> for Hex {
+    type Output = Hex;
+
+    fn add(self, _rhs: Hex) -> Hex {
+        Hex::new(self.x + _rhs.x, self.y + _rhs.y)
+    }
+}
+impl ops::Add<&Hex> for Hex {
+    type Output = Hex;
+    fn add(self, _rhs: &Hex) -> Hex {
+        Hex::new(self.x + _rhs.x, self.y + _rhs.y)
+    }
+}
+// pub type Hex = (i32, i32);
 
 peg::parser! {
   grammar instructions_parser() for str {
 
     rule east() -> Hex
-        = "e" { (1, 0) }
+        = "e" { Hex::new(1, 0) }
 
     rule south_east() -> Hex
-        = "se" { (0, 1) }
+        = "se" { Hex::new(0, 1) }
 
     rule south_west() -> Hex
-        = "sw" { (-1, 1)  }
+        = "sw" { Hex::new(-1, 1)  }
 
     rule west() -> Hex
-        = "w" { (-1, 0) }
+        = "w" { Hex::new(-1, 0) }
 
     rule north_west() -> Hex
-        = "nw" { (0, -1) }
+        = "nw" { Hex::new(0, -1) }
 
     rule north_east() -> Hex
-        = "ne" { (1, -1)}
+        = "ne" { Hex::new(1, -1)}
 
     pub rule item() -> Hex
       = n: (east() / south_east() / south_west() /  west() / north_west() / north_east()) {
@@ -53,19 +82,48 @@ peg::parser! {
 
 pub fn run() {
     let mut tiles: HashMap<Hex, u32> = HashMap::new();
-    let mut iter = include_str!("../day24.txt").split("\n").for_each(|line| {
-        let tile0: Hex = (0, 0);
-        println!("parsing {}", line);
+    include_str!("../day24.txt").split("\n").for_each(|line| {
+        let tile0: Hex = Hex::new(0, 0);
         let dst_tile = instructions_parser::row(line)
             .ok()
             .unwrap()
             .iter()
-            .fold(tile0, |acc, step| (acc.0 + step.0, acc.1 + step.1));
+            .fold(tile0, |acc, step| acc + step);
         *tiles.entry(dst_tile).or_insert(0) += 1;
     });
 
     let count = tiles.values().filter(|&&v| v % 2 == 1).count();
-    println!("{}", count)
+    println!("{}", count);
+
+    for i in 1..101 {
+        let mut new_tiles: HashMap<Hex, u32> = HashMap::new();
+        // println!("size {}", tiles.len());
+        tiles
+            .keys()
+            .flat_map(|&v| v.neighbours().into_iter())
+            .unique()
+            .for_each(|t| {
+                let is_black = tiles.get(&t).unwrap_or(&0) % 2 == 1;
+                let black_neighbours = t
+                    .neighbours()
+                    .iter()
+                    .filter(|&t| tiles.get(t).unwrap_or(&0) % 2 == 1)
+                    .count();
+                // println!("BLACK {}  {}", is_black, black_neighbours);
+                if is_black && (black_neighbours == 0 || black_neighbours > 2) {
+                    new_tiles.insert(t, 0);
+                } else if !is_black && black_neighbours == 2 {
+                    new_tiles.insert(t, 1);
+                } else {
+                    let v = if is_black { 1 } else { 0 };
+                    new_tiles.insert(t, v);
+                }
+            });
+        tiles = new_tiles;
+    }
+
+    let count = tiles.values().filter(|&&v| v % 2 == 1).count();
+    println!("{}", count);
 }
 
 #[cfg(test)]
@@ -77,8 +135,8 @@ mod tests {
         let tile: Vec<Hex> = instructions_parser::row("e").ok().unwrap();
 
         assert_eq!(1, tile.len());
-        assert_eq!(1, tile.get(0).unwrap().0);
-        assert_eq!(0, tile.get(0).unwrap().1);
+        assert_eq!(1, tile.get(0).unwrap().x);
+        assert_eq!(0, tile.get(0).unwrap().y);
 
         println!("{:?}", tile);
     }
